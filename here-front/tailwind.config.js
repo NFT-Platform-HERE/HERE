30e6df7ff5d6483ed349073fde123270b10e7e8e
최정온 <chjo0330@gmail.com>
2023-03-14 16:18:24 +0900

    FE-3001 chore: Tailwind Color 추가

--- here-front/tailwind.config.js
+++ here-front/tailwind.config.js
@@ -1,9 +1,9 @@
-const px_to_rem = (x) => { return { ...Array.from(Array(x+1)).map((_, i) => `${i / 16}rem`) } };
+const px_to_rem = (x) => {
+  return { ...Array.from(Array(x + 1)).map((_, i) => `${i / 16}rem`) };
+};
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,js,ts,jsx,tsx}"
-  ],
+  content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       borderWidth: px_to_rem(100),
@@ -17,22 +17,27 @@ module.exports = {
       minWidth: px_to_rem(300),
       minHeight: px_to_rem(300),
       spacing: px_to_rem(50),
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        white: "#ffffff",
+        "red-0": "#ede1e1",
+        "red-1": "#ff9999",
+        "red-2": "#ff5050",
+        "red-3": "#ff2d54",
+        "pink-0": "#fff6f6",
+        "pink-1": "#ffe4e4",
+        "pink-2": "#fadbda",
+        "pen-0": "#e5e5e5",
+        "pen-1": "#9f9f9f",
+        "pen-2": "#655f5f",
+        "pen-3": "#443B3B",
+        "pen-4": "#868E96",
+      },
+      screens: {
+        mobile: { max: "480px" },
+      },
     },
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'white': '#ffffff',
-      'red-0': '#ede1e1', 
-      'red-1': '#ff9999',
-      'red-2': '#ff5050',
-      'red-3': '#ff2d54',
-      'pink-0': "#fff6f6",
-      'pink-1': "#ffe4e4",
-      'pink-2': "#fadbda",
-      'pen-0': "#e5e5e5",
-      'pen-1': "#9f9f9f",
-      'pen-2': "#655f5f"
-    }
   },
   plugins: [],
-}
+};
