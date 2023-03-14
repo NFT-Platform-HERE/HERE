157f56fff071bb0ef61b105cf249041cd5875ab4
skylove0911 <skylove0911@naver.com>
2023-03-14 17:51:59 +0900

    FE-1001 feat: commonBanner 구현

--- here-front/src/components/Banner/CommonBanner.tsx
+++ here-front/src/components/Banner/CommonBanner.tsx
@@ -1,7 +1,22 @@
-export default function CommonBanner() {
-  return (
-    <button className=''>
+interface Iprops {
+  bgColor: string;
+  imgUrl: string;
+}
 
-    </button>
-  )
+export default function CommonBanner({ bgColor, imgUrl }: Iprops) {
+  return (
+    <div className="flex h-350 w-full flex-col items-center">
+      <div
+        className="h-300 w-full min-w-[1200px]"
+        css={[
+          {
+            backgroundColor: bgColor,
+          },
+        ]}
+      ></div>
+      <div className="-mt-[250px] h-300 w-[1200px]">
+        <img src={imgUrl} className="h-300 w-[1200px]" />
+      </div>
+    </div>
+  );
 }
--- here-front/src/components/Box/InputBox/CommonInputBox.tsx
+++
@@ -1,7 +0,0 @@
-export default function CommonInputBox() {
-  return (
-    <button className=''>
-
-    </button>
-  )
-}
