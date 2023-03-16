360bf92121e2f50511305b4222394d505e7ffd15
skylove0911 <skylove0911@naver.com>
2023-03-16 17:57:54 +0900

    FE-1001 feat: WebHeader 완료 mobileHeader 작업 중

--- here-front/src/components/Header/MobileHeader.tsx
+++ here-front/src/components/Header/MobileHeader.tsx
@@ -1,7 +1,41 @@
-export default function MobileHeader() {
-  return (
-    <button className=''>
+import { useState } from "react";
+import MobileHeaderMenuDropdown from "./MobileHeaderMenuDropdown";
+import MobileHeaderProfileDropdown from "./MobileHeaderProfileDropdown";
 
-    </button>
-  )
+export default function MobileHeader() {
+  const [menuDropDown, setMenuDropDown] = useState<boolean>(false);
+  const [profileDropDown, setProfileDropDown] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
+
+  const handleMenuDropDown = () => {
+    setMenuDropDown(!menuDropDown);
+    setProfileDropDown(false);
+  };
+
+  const handleProfileDropDown = () => {
+    setMenuDropDown(false);
+    setProfileDropDown(!profileDropDown);
+  };
+
+  return (
+    <div className="ml-10 flex h-60 w-[calc(100%-20px)] min-w-300 items-center justify-between">
+      <img
+        src="/icons/character.svg"
+        className="h-35 w-35"
+        onClick={handleProfileDropDown}
+      ></img>
+      <img src="/icons/logo.svg" className="h-40 w-80"></img>
+      <img
+        src="/icons/menu.svg"
+        className="h-30 w-30"
+        onClick={handleMenuDropDown}
+      ></img>
+      <div className="absolute top-60 left-0 h-270 w-full">
+        {profileDropDown && <MobileHeaderProfileDropdown />}
+      </div>
+      <div className="absolute top-60 right-0 h-270 w-full">
+        {menuDropDown && <MobileHeaderMenuDropdown />}
+      </div>
+    </div>
+  );
 }
--- here-front/src/components/Header/WebHeader.tsx
+++ here-front/src/components/Header/WebHeader.tsx
@@ -1,44 +1,48 @@
-import { useRouter } from "next/navigation";
-import { useState } from "react";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
 import WebHeaderDropdown from "./WebHeaderDropdown";
 
 export default function WebHeader() {
+  const [hashTag, setHashTag] = useState<boolean>(false);
   const [dropDown, setDropDown] = useState<boolean>(false);
+  const [isDisabled, setIsDisabled] = useState<boolean>(true);
 
   const router = useRouter();
+
+  const movePage = (path: string) => {
+    router.push(path);
+    setDropDown(false);
+  };
 
   const handleDropDown = () => {
     setDropDown(!dropDown);
   };
 
-  return (
-    <div className="justify-content min-w-900 flex h-65 w-full justify-center shadow-sm">
+  useEffect(() => {
+    setIsDisabled(
+      !(router.asPath !== "/organization" && router.asPath !== "/redcross"),
+    );
+  }, []);
+
+  return isDisabled ? null : (
+    <div className="justify-content flex h-65 w-full min-w-[1200px] justify-center shadow-sm">
       <div className="relative flex h-65 w-[1200px] justify-between">
         <img
           src="/icons/logo.svg"
           className="cursor-pointer"
-          onClick={() => router.push("/")}
+          onClick={() => movePage("/")}
         ></img>
         <div className="flex h-65 w-665 items-center justify-around text-18 font-medium">
-          <div
-            className="cursor-pointer"
-            onClick={() => router.push("/donate")}
-          >
+          <div className="cursor-pointer" onClick={() => movePage("/donate")}>
             기부해요
           </div>
-          <div className="cursor-pointer" onClick={() => router.push("/blood")}>
+          <div className="cursor-pointer" onClick={() => movePage("/blood")}>
             헌혈해요
           </div>
-          <div
-            className="cursor-pointer"
-            onClick={() => router.push("/submit")}
-          >
+          <div className="cursor-pointer" onClick={() => movePage("/submit")}>
             제출해요
           </div>
-          <div
-            className="cursor-pointer"
-            onClick={() => router.push("/my-nft")}
-          >
+          <div className="cursor-pointer" onClick={() => movePage("/my-nft")}>
             나의 NFT
           </div>
         </div>
