e2f800b986482532b0a0c839cbc4a5beec3c5fa3
eondo <vov06144@naver.com>
2023-03-17 00:55:40 +0900

    BE-1003 feat: auth 관련 controller 생성

--- back/here-auth/src/main/java/com/ssafy/hereauth/controller/MemberController.java
+++ back/here-auth/src/main/java/com/ssafy/hereauth/controller/MemberController.java
@@ -2,16 +2,14 @@ package com.ssafy.hereauth.controller;
 
 import com.ssafy.hereauth.dto.common.response.ResponseSuccessDto;
 import com.ssafy.hereauth.dto.member.SignupRequestDto;
+import com.ssafy.hereauth.dto.member.SignupResponseDto;
 import com.ssafy.hereauth.service.MemberService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Api("Member Controller v1")
 @RestController
@@ -22,9 +20,16 @@ public class MemberController {
 
     private final MemberService memberService;
 
+    /* 회원가입 */
     @ApiOperation(value = "회원가입", notes = "회원가입을 한다.")
     @PostMapping("/signup")
-    public ResponseEntity<ResponseSuccessDto<String>> signup(@RequestBody SignupRequestDto signupRequestDto) {
+    public ResponseEntity<ResponseSuccessDto<SignupResponseDto>> signup(SignupRequestDto signupRequestDto) {
         return ResponseEntity.ok(memberService.signup(signupRequestDto));
+    }
+
+    @ApiOperation(value = "이메일 중복 검사", notes = "이메일 중복 검사를 한다.")
+    @GetMapping("/check/email/{email}")
+    public ResponseEntity<ResponseSuccessDto<Boolean>> checkEmailDuplicate(@PathVariable String email) {
+        return ResponseEntity.ok(memberService.checkEmailDuplicate(email));
     }
 }
