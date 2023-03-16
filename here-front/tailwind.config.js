6ac70dbfcc431d422d3a99a786a2617b4667f73a
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-03-16 14:05:05 +0900

    Fix : merge confilct 해결

--- here-front/tailwind.config.js
+++ here-front/tailwind.config.js
@@ -21,7 +21,6 @@ module.exports = {
         mobile: { max: "480px" },
       },
       colors: {
-<<<<<<< HEAD
         transparent: 'transparent',
         current: 'currentColor',
         'black': "#000000",
@@ -35,23 +34,9 @@ module.exports = {
         'pink-2': "#fadbda",
         'pen-0': "#e5e5e5",
         'pen-1': "#9f9f9f",
-        'pen-2': "#655f5f"
-=======
-        black: "#000000",
-        white: "#ffffff",
-        "red-0": "#ede1e1",
-        "red-1": "#ff9999",
-        "red-2": "#ff5050",
-        "red-3": "#ff2d54",
-        "pink-0": "#fff6f6",
-        "pink-1": "#ffe4e4",
-        "pink-2": "#fadbda",
-        "pen-0": "#e5e5e5",
-        "pen-1": "#9f9f9f",
-        "pen-2": "#655f5f",
+        'pen-2': "#655f5f",
         "pen-3": "#443B3B",
         "pen-4": "#868E96",
->>>>>>> ec4b96d11566d516bcb67799bec768a495fe1af2
       },
     },
   },
