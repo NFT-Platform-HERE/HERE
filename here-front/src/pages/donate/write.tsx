207c6c1a7af607408f2ca6d729ff5e74a70d1d30
최정온 <chjo0330@gmail.com>
2023-03-16 20:01:19 +0900

    FE-3003 feat: DonateWritePage 작업 중

--- here-front/src/pages/donate/write.tsx
+++ here-front/src/pages/donate/write.tsx
@@ -1,3 +1,46 @@
+import CommonBtn from "@/components/Button/CommonBtn";
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 export default function DonateWritePage() {
-  return <div>기부 글 작성 페이지</div>;
+  const [value, setValue] = useState("");
+
+  function printVal() {
+    console.log("value", value);
+  }
+
+  return (
+    <div className="mt-25 w-full">
+      <div className="grid grid-cols-8 gap-2">
+        <div className="mobile:hidden"></div>
+        <div className="col-span-6 mobile:col-span-8">
+          <div className="grid grid-cols-8 gap-2">
+            <div className="mobile:hidden"></div>
+            <div className="col-span-6 mobile:col-span-8">
+              <div className="mr-10 mt-5 mb-10 flex justify-end mobile:hidden">
+                <CommonBtn
+                  width={95}
+                  height={50}
+                  fontSize={20}
+                  children={"등록"}
+                  isDisabled={false}
+                  onClick={() => {}}
+                />
+              </div>
+              <input
+                type="text"
+                placeholder="제목을 입력하세요"
+                className="text-20 text-pen-2 outline-none mobile:text-13"
+              />
+              <button onClick={printVal}>내용 확인</button>
+              <ReactQuill theme="snow" value={value} onChange={setValue} />
+            </div>
+            <div className="mobile:hidden"></div>
+          </div>
+        </div>
+        <div className="mobile:hidden"></div>
+      </div>
+    </div>
+  );
 }
