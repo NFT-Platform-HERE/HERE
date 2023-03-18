4d2f4a4e7c7a49c5d5ebdd8bce889e6ec9d7859c
최정온 <chjo0330@gmail.com>
2023-03-18 15:30:15 +0900

    FE-3003 feat: DonateWritePage 작업 중

--- here-front/src/pages/donate/write.tsx
+++ here-front/src/pages/donate/write.tsx
@@ -36,9 +36,33 @@ export default function DonateWritePage() {
               <input
                 type="text"
                 placeholder="제목을 입력하세요"
-                className="text-20 text-pen-2 outline-none mobile:text-13"
+                className="mb-33 text-20 text-pen-2 outline-none mobile:text-13"
               />
-              <button onClick={printVal}>내용 확인</button>
+              <div className="mb-5 flex items-center justify-start">
+                <div className="mr-31 text-18 font-normal text-pen-2">
+                  * 목표수량
+                </div>
+                <div className="mr-8 flex h-55 w-85 items-center justify-center rounded-60 border border-pen-0 text-18 font-normal text-pen-2">
+                  30
+                </div>
+                {/* <div className="mx-3 flex h-50 w-50 items-center justify-center rounded-50 border border-pen-0 text-30 font-normal text-pen-2">
+                  +
+                </div>
+                <div className="mx-3 flex h-50 w-50 items-center justify-center rounded-50 border border-pen-0 text-30 font-normal text-pen-2">
+                  -
+                </div> */}
+                <img src={"/icons/add-circle-button.svg"} />
+                <img src={"/icons/minus-circle-button.svg"} />
+              </div>
+              <div className="mb-25 flex items-center justify-start">
+                <div className="mr-31 text-18 font-normal text-pen-2">
+                  * 마감기한
+                </div>
+                <div className="mr-8 flex h-55 w-85 items-center justify-center rounded-60 border border-pen-0 text-18 font-normal text-pen-2">
+                  30
+                </div>
+              </div>
+              {/* <button onClick={printVal}>내용 확인</button> */}
               <QuillWrapper theme="snow" value={value} onChange={setValue} />
             </div>
             <div className="mobile:hidden"></div>
