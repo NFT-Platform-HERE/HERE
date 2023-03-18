b272f075b1e1eaaff6700493ecf9f55fc79a7838
최정온 <chjo0330@gmail.com>
2023-03-18 15:36:30 +0900

    Chore: favicon 적용

--- here-front/src/pages/_app.tsx
+++ here-front/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import "../index.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import Header from "@/components/Header/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
