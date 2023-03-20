5158fa82d3afc966f6d731f4e44e0e8e35dfc647
skylove0911 <skylove0911@naver.com>
2023-03-20 17:57:50 +0900

    FE-1002 feat: 나의 NFT 페이지 완성

--- here-front/src/pages/my-nft/index.tsx
+++ here-front/src/pages/my-nft/index.tsx
@@ -1,35 +1,169 @@
-import NFTCard from "@/components/Card/NFTCard";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import { EffectCoverflow, Navigation } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
+import NFTCardFront from "@/components/Card/NFTCardFront";
+import NFTCardBack from "@/components/Card/NFTCardBack";
+import { useEffect, useState } from "react";
+import Background from "@/components/Background/Background";
+import InstaBtn from "@/components/Button/InstaBtn";
+
+const swiperList = [
+  {
+    index: 2,
+  },
+  {
+    index: 3,
+  },
+  {
+    index: 4,
+  },
+  {
+    index: 5,
+  },
+];
+
+const swiperStyle = `            
+  .swiper-wrapper{
+    width: 100%;
+    height: 50%;
+  }
+  .swiper-button-prev{
+    width: 30px;
+    height: 30px;
+    top: 37px;
+    left: 55px;
+    color: #ff5050;
+    transform:rotate(90deg);
+  }
+  .swiper-button-next{         
+    width: 30px;
+    height: 30px;
+    top: 615px;
+    left: 55px;
+    color: #ff5050;
+    transform:rotate(90deg);
+  }`;
+
+const navigatorStyle = `
+  .swiper-button-prev{
+    width: 30px;
+    height: 30px;
+    left: 0px;
+    color: #ff5050;
+  }
+  .swiper-button-next{         
+    width: 30px;
+    height: 30px;
+    right: 0px;
+    color: #ff5050;
+  }`;
 
 export default function MyNFTPage() {
-  return (
-    <div className="w-min-[1200px] flex">
-      <div className="mt-300 h-153 w-720 rotate-90">
-        <Swiper
-          spaceBetween={10}
-          slidesPerView={3}
-          navigation={true}
-          modules={[Navigation]}
-        >
-          <SwiperSlide>
-            <div className="h-137 w-214 bg-red-1">Slide 1</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-137 w-214 bg-blue-100">Slide 2</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-137 w-214 bg-purple-100">Slide 3</div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="h-137 w-214 bg-yellow-100">Slide 4</div>
-          </SwiperSlide>
-        </Swiper>
-      </div>
+  const [selectedCardList, setSelectedCardList] = useState<number[]>([]);
+  const [NFTCardBackIndex, setNFTCardBackIndex] = useState<number>(0);
 
-      <div></div>
+  const handleSetSelectedCardList = (index: number) => {
+    if (selectedCardList.includes(index)) {
+      setSelectedCardList(
+        selectedCardList.filter((selectedCard) => {
+          return index !== selectedCard;
+        }),
+      );
+    } else {
+      setSelectedCardList([...selectedCardList, index]);
+    }
+  };
+
+  useEffect(() => {
+    if (swiperList) {
+      setSelectedCardList([swiperList[0].index]);
+    }
+  }, []);
+
+  return (
+    <div className="w-min-[1200px] relative">
+      <div className="flex w-full min-w-[1200px] justify-center">
+        <div className="flex w-[1200px] items-center justify-between">
+          <div className="relative flex h-630 w-137 items-center justify-center">
+            <Swiper
+              slidesPerView={3}
+              navigation={true}
+              modules={[Navigation]}
+              direction={"vertical"}
+              className="static flex h-520 w-137"
+              css={[swiperStyle]}
+            >
+              {swiperList.map((item, index) => (
+                <SwiperSlide
+                  className="relative mt-8 -mb-8 flex justify-center"
+                  key={index}
+                  onClick={() => handleSetSelectedCardList(item.index)}
+                >
+                  <NFTCardFront
+                    width={100}
+                    imgUrl={`/NFT_bg_${item.index}.gif`}
+                  />
+
+                  {selectedCardList.includes(item.index) && (
+                    <img
+                      src="/icons/check.svg"
+                      className="absolute -top-8 right-15 h-25 w-25"
+                    />
+                  )}
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+
+          <div className="relative flex w-983 justify-center gap-43">
+            <div className="absolute -top-50 right-0">
+              <InstaBtn
+                width={195}
+                height={40}
+                onClick={() => console.log("click!")}
+                fontSize={18}
+              >
+                SNS 공유하기
+              </InstaBtn>
+            </div>
+            <Swiper
+              slidesPerView={3}
+              navigation={true}
+              centeredSlides={true}
+              modules={[Navigation]}
+              className="static flex w-900 items-center justify-center"
+              css={[navigatorStyle]}
+            >
+              {selectedCardList.map((item, index) => (
+                <SwiperSlide className="z-0 flex items-center justify-center">
+                  {({ isActive }) => (
+                    <div
+                      onClick={() => setNFTCardBackIndex(item)}
+                      className={isActive ? "z-20" : "blur-sm"}
+                    >
+                      <NFTCardFront
+                        width={isActive ? 300 : 250}
+                        key={index}
+                        imgUrl={`/NFT_bg_${item}.gif`}
+                      />
+                    </div>
+                  )}
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        </div>
+      </div>
+      {NFTCardBackIndex !== 0 && (
+        <div>
+          <Background onClick={() => setNFTCardBackIndex(0)} />
+
+          <div className="fixed top-[calc(50%-175px)] left-[calc(50%-283px)] z-30">
+            <NFTCardBack height={350} fontSize={18}></NFTCardBack>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
