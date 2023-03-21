33f565634c96f9f35600457fd43f21abc7406b8f
choi1087 <adcdm87@gmail.com>
2023-03-21 10:20:56 +0900

    BE-3006 feat: create here-nft project

---
+++ here-back/here-nft/src/main/java/com/ssafy/herenft/HereNftApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.herenft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereNftApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HereNftApplication.class, args);
+    }
+
+}
