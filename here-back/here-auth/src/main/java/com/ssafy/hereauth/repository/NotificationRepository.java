3094c302cec831dfd40a23341e7a969e2d398234
choi1087 <adcdm87@gmail.com>
2023-03-21 17:54:52 +0900

    BE-3005 feat: add notification service

---
+++ here-back/here-auth/src/main/java/com/ssafy/hereauth/repository/NotificationRepository.java
@@ -0,0 +1,15 @@
+package com.ssafy.hereauth.repository;
+
+import com.ssafy.hereauth.entity.Member;
+import com.ssafy.hereauth.entity.Notification;
+import com.ssafy.hereauth.enumeration.EnumNotificationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findAllByReceiverAndStatus(Member receiver, EnumNotificationStatus status);
+    Optional<Notification> findByIdAndReceiver(Long notificationId, Member Receiver);
+}
