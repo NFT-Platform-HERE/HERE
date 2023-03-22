23742ab9fac1f7947e27a6d851875f51e07e9e88
최정온 <chjo0330@gmail.com>
2023-03-23 00:37:16 +0900

    FE-3002 feat: DonateTitle 수정

--- here-front/src/features/Donate/DonateTitle.tsx
+++ here-front/src/features/Donate/DonateTitle.tsx
@@ -4,7 +4,7 @@ interface Iprops {
 
 export default function DonateTitle({ title }: Iprops) {
   return (
-    <div className="flex justify-center text-22 font-bold text-pen-2 mobile:mb-14 mobile:text-16">
+    <div className="flex justify-center text-22 font-bold text-pen-2 mobile:mb-3 mobile:text-16">
       {title}
     </div>
   );
