b1f85a690962feb671b298f11ade4f4f7ebc5f8d
gusrnqq@naver.com <gusrnqq@naver.com>
2023-03-22 17:52:11 +0900

    BE-2005 feat: HereNFT 수정

--- here-blockchain/contracts/HereNFT.sol
+++ here-blockchain/contracts/HereNFT.sol
@@ -105,4 +105,9 @@ contract HereNFT is ERC721 {
         emit createNFT(newItemId, to);
         return newItemId;
     }
+
+    function verifyNFT(uint256 tokenId, bytes32 message, uint8 v, bytes32 r, bytes32 s) public view returns (bool) {
+        address signer = ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", message)), v, r, s);
+        return signer == ownerOf(tokenId);
+    }
 }
