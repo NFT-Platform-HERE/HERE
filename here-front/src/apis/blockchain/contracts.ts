653b33c51d140f704505a13eb88f453f5e6bbcf5
최정온 <chjo0330@gmail.com>
2023-03-24 00:15:13 +0900

    FE-3004 feat: 블록체인 설정 업데이트

--- here-front/src/apis/blockchain/contracts.ts
+++ here-front/src/apis/blockchain/contracts.ts
@@ -1,6 +1,10 @@
-import { hereContract } from "../../constants/web3Config";
+import Web3 from "web3";
+import { HERE_ERC_721_ABI, HERE_ERC_721_CA } from "@/constants/blockchain";
 
 export const mintBloodNFT = async (account: string, metadata: string) => {
+  const web3 = new Web3(window.ethereum);
+  const hereContract = new web3.eth.Contract(HERE_ERC_721_ABI, HERE_ERC_721_CA);
+
   const result = await hereContract.methods
     .create(account, metadata)
     .send({ from: account });
--- here-front/src/constants/web3Config.ts
+++ here-front/src/constants/web3Config.ts
@@ -1,14 +1,6 @@
-import Web3 from "web3";
 import { create } from "ipfs-http-client";
-import { HERE_ERC_721_ABI, HERE_ERC_721_CA } from "@/constants/blockchain";
 import { IPFS_SERVER_URL } from "../utils/urls";
 
-export const web3 = new Web3(window.ethereum);
-
 export const ipfs = create({ url: IPFS_SERVER_URL });
-// export const ipfs = create({ url: process.env.NEXT_PUBLIC_IPFS_SERVER_URL });
 
-export const hereContract = new web3.eth.Contract(
-  HERE_ERC_721_ABI,
-  HERE_ERC_721_CA,
-);
+// export const ipfs = create({ url: process.env.NEXT_PUBLIC_IPFS_SERVER_URL });
