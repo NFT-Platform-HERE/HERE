2ab7e110bca02424c6e6a50cce7c849d6e968762
최정온 <chjo0330@gmail.com>
2023-03-23 16:44:55 +0900

    FE-3004 feat: web3Config Update

--- here-front/src/constants/web3Config.ts
+++ here-front/src/constants/web3Config.ts
@@ -1,9 +1,13 @@
 import Web3 from "web3";
+import { create } from "ipfs-http-client";
 import { HERE_ERC_721_ABI, HERE_ERC_721_CA } from "@/constants/blockchain";
 
 export const web3 = new Web3(window.ethereum);
+
+export const ipfs = create({ url: process.env.NEXT_PUBLIC_IPFS_SERVER_URL });
 
 export const hereContract = new web3.eth.Contract(
   HERE_ERC_721_ABI,
   HERE_ERC_721_CA,
 );
+
