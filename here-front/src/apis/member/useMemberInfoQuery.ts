8196742b6714ee3f44b08d4a03196dc9fc1d9234
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-03-24 17:13:39 +0900

    FE-2008 feat: 멤버 명함 조회 API 구현 & 헌혈해요 UI 수정

--- here-front/src/apis/member/useMemberInfoQuery.ts
+++ here-front/src/apis/member/useMemberInfoQuery.ts
@@ -3,10 +3,12 @@ import axios from "axios";
 import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
 
-const fetcher = (memberId: number) =>
-  axios.get(MEMBER_SERVER_URL + `/member/${memberId}`).then(({ data }) => data);
+const fetcher = (memberId: string) =>
+  axios
+    .get(MEMBER_SERVER_URL + `/member/${memberId}`)
+    .then(({ data }) => data.data);
 
-const useMemberInfoQuery = (memberId: number) => {
+const useMemberInfoQuery = (memberId: string) => {
   return useQuery(queryKeys.MEMBER_INFO, () => fetcher(memberId));
 };
 
