de785b1aee7e13cf39b458addef61ebe2a86c439
skylove0911 <skylove0911@naver.com>
2023-03-27 00:32:19 +0900

    FE-1008 feat: react-qr-reader 테스트

--- here-front/src/pages/register/index.tsx
+++ here-front/src/pages/register/index.tsx
@@ -1,24 +1,20 @@
 import { useState } from "react";
 import dynamic from "next/dynamic";
-import QrReader from "react-qr-scanner";
-import { useQRCode } from "next-qrcode";
-
-const BarcodeScannerComponent = dynamic(
-  () => import("react-qr-barcode-scanner"),
-);
+import { QrReader } from "react-qr-reader";
 
 export default function RegisterPage() {
   const [data, setData] = useState("Not Found");
 
   return (
     <>
-      <BarcodeScannerComponent
-        width={500}
-        height={500}
-        onUpdate={(err, result: any) => {
-          if (result) setData(result.text);
-          else setData("Not Found");
+      <QrReader
+        onScan={(data) => {
+          setData(data);
         }}
+        onError={(error) => {
+          console.log(error);
+        }}
+        style={{ width: "500px" }}
       />
       <p>{data}</p>
     </>
