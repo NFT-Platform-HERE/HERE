576ac386665536dbb2206e00051efbd9e9467726
최정온 <chjo0330@gmail.com>
2023-03-28 10:58:53 +0900

    FE-3007 feat: useDonateDetailQuery 수정

--- here-front/src/apis/donate/useDonateDetailQuery.ts
+++ here-front/src/apis/donate/useDonateDetailQuery.ts
@@ -2,7 +2,6 @@ import { DONATE_SERVER_URL } from "@/utils/urls";
 import axios from "axios";
 import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
-import { Donation } from "./../../types/Donation";
 
 interface Iprops {
   boardId: number;
@@ -10,11 +9,11 @@ interface Iprops {
 
 const fetcher = (boardId: number) =>
   axios.get(DONATE_SERVER_URL + `/board/${boardId}`).then(({ data }) => {
-    const response = data.data as Donation[];
+    const response = data.data;
     return response;
   });
 
-const useDonateDetailQuery = ({boardId}: Iprops) => {
+const useDonateDetailQuery = ({ boardId }: Iprops) => {
   return useQuery([queryKeys.DONATE_DETAIL, boardId], () => fetcher(boardId), {
     suspense: true,
   });
