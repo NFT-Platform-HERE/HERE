05900fd9eabf4a9ffec01483a3150de35c3e36d0
eondo <vov06144@naver.com>
2023-03-30 13:52:41 +0900

    BE-1003 refactor: CertHistory 레포지토리 삭제

--- here-back/here-auth/src/main/java/com/ssafy/hereauth/repository/CertHistoryRepository.java
+++
@@ -1,6 +0,0 @@
-package com.ssafy.hereauth.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CertHistoryRepository extends JpaRepository<CertHistory, Long> {
-}
