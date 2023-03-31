326c42d8f81994a88d26f4b32ae9aa3e9eb6f99f
eondo <vov06144@naver.com>
2023-03-31 16:36:20 +0900

    BE-1007 refactor: request dto에서 예외 케이스 annotation 추가

--- here-back/here-nft/src/main/java/com/ssafy/herenft/dto/nft/TransferOwnershipRequestDto.java
+++ here-back/here-nft/src/main/java/com/ssafy/herenft/dto/nft/TransferOwnershipRequestDto.java
@@ -3,13 +3,19 @@ package com.ssafy.herenft.dto.nft;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
 @Builder
 public class TransferOwnershipRequestDto {
+    @NotNull(message = "송신자 멤버 ID는 필수 값입니다.")
     private UUID senderId;
+    @NotNull(message = "수신자 멤버 ID는 필수 값입니다.")
     private UUID receiverId;
+    @NotNull(message = "nft 토큰 목록은 필수 값입니다.")
+    @NotEmpty(message = "nft 토큰 목록을 선택해주세요.")
     private List<Long> nftTokenList;
 }
