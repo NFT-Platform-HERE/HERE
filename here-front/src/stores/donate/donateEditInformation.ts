a736c6d1b01652b066787986428415e7fd0aeb88
최정온 <chjo0330@gmail.com>
2023-04-01 17:22:30 +0900

    FE-3013 feat: donateSlice 작성

---
+++ here-front/src/stores/donate/donateEditInformation.ts
@@ -0,0 +1,50 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface donateEditInfo {
+  boardId: number;
+  boardImgUrlList: string[];
+  content: string;
+  goalQuantity: number;
+  title: string;
+  memberId: string;
+  deadline: string;
+}
+
+const initialState: donateEditInfo = {
+  title: "",
+  boardId: 0,
+  boardImgUrlList: [],
+  content: "",
+  goalQuantity: 0,
+  memberId: "",
+  deadline: "",
+};
+
+const donateSlice = createSlice({
+  name: "donate",
+  initialState,
+  reducers: {
+    getBoardEditInfo(state, action) {
+      state.boardId = action.payload.boardId;
+      state.boardImgUrlList = action.payload.boardImgUrlList;
+      state.content = action.payload.content;
+      state.goalQuantity = action.payload.goalQuantity;
+      state.title = action.payload.title;
+      state.memberId = action.payload.memberId;
+      state.deadline = action.payload.deadline;
+    },
+    clearBoardEditInfo(state) {
+      state.title = "";
+      state.boardId = 0;
+      state.boardImgUrlList = [];
+      state.content = "";
+      state.goalQuantity = 0;
+      state.memberId = "";
+      state.deadline = "";
+    },
+  },
+});
+
+export const { getBoardEditInfo, clearBoardEditInfo } = donateSlice.actions;
+
+export default donateSlice.reducer;
--- here-front/src/stores/store.ts
+++ here-front/src/stores/store.ts
@@ -12,6 +12,7 @@ import submitTabSlice from "./submit/submitTab";
 import submitSelectedHospitalNFTSlice from "./submit/selectedHospitalNFT";
 import submitSelectedOrganizationNFTSlice from "./submit/selectedOrganizationNFT";
 import clickAutoSelectBtnSlice from "./submit/clickAutoSelectBtn";
+import donateSlice from "./donate/donateEditInformation";
 
 const reducers = combineReducers({
   myNFT: myNFTSlice,
@@ -25,6 +26,7 @@ const reducers = combineReducers({
   submitSelectedOrganizationNFT: submitSelectedOrganizationNFTSlice,
   webHeaderDropdown: webHeaderDropdownSlice,
   clickAutoSelectBtn: clickAutoSelectBtnSlice,
+  donate: donateSlice,
 });
 
 const persistConfig = {
