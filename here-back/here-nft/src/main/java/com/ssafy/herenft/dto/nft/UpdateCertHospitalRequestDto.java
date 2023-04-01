36ab1ad5e12126d76f2d06ab37919493fc587e92
eondo <vov06144@naver.com>
2023-04-02 00:30:23 +0900

    BE-1007 fix: 증명서 승인 여부 갱신(병원) 에러 수정

--- here-back/here-nft/src/main/java/com/ssafy/herenft/dto/nft/UpdateCertHospitalRequestDto.java
+++ here-back/here-nft/src/main/java/com/ssafy/herenft/dto/nft/UpdateCertHospitalRequestDto.java
@@ -1,12 +1,10 @@
 package com.ssafy.herenft.dto.nft;
 
-import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
-@Builder
 public class UpdateCertHospitalRequestDto {
     private List<Long> tokenIdList;
 }
