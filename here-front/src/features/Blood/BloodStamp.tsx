efe5e7c6a3678d0e8255633cdd18c77269a77f2e
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-04-02 03:02:25 +0900

    FE-2016 feat: 스탬프 자리 만들어주기.....

--- here-front/src/features/Blood/BloodStamp.tsx
+++ here-front/src/features/Blood/BloodStamp.tsx
@@ -8,9 +8,12 @@ export default function BloodStamp() {
   const stamp = useStampQuery(memberId);
   console.log("stamp", stamp.data);
   return (
-    <div>
-      <div className="h-60 w-300 rounded-50 border-5 border-red-2 bg-white text-center text-20 font-semibold leading-50 text-red-2 mobile:h-50 mobile:w-[calc(100%-70px)] mobile:text-16 mobile:leading-40">
+    <div className="mb-100">
+      <div className="relative z-20 mx-auto -mb-30 h-60 w-300 rounded-50 border-5 border-red-2 bg-white text-center text-20 font-semibold leading-50 text-red-2 mobile:h-50 mobile:w-[calc(100%-70px)] mobile:text-16 mobile:leading-40">
         헌혈 스탬프
+      </div>
+      <div className="relative z-10 h-500 w-1100 rounded-30 border-3 border-pen-5 ">
+        스탬프 디자인 어캐함요;;
       </div>
     </div>
   );
