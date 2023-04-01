737a6339040aaeb7b5e2450ffcc04bc99522ef0f
최정온 <chjo0330@gmail.com>
2023-04-01 16:07:22 +0900

    FE-3013 feat: 기부/마감 후 ReRendering

--- here-front/src/apis/donate/useDonateNftWrite.ts
+++ here-front/src/apis/donate/useDonateNftWrite.ts
@@ -1,7 +1,8 @@
 import { NFT_SERVER_URL } from "@/utils/urls";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import axios from "axios";
 import { DonationNft } from "@/types/DonationNft";
+import * as queryKeys from "@/constants/queryKeys";
 
 const fetcher = (payload: DonationNft) =>
   axios
@@ -14,9 +15,11 @@ const fetcher = (payload: DonationNft) =>
     .then(({ data }) => data);
 
 const useDonateNftWrite = () => {
+  const queryClient = useQueryClient();
   return useMutation(fetcher, {
     onSuccess: (data) => {
       console.log("성공!");
+      return queryClient.invalidateQueries(queryKeys.DONATE_DETAIL);
     },
     onError: () => {
       console.log("onERROR");
