0b97311efc2867ac2a05c2646d395d39703ee342
최정온 <chjo0330@gmail.com>
2023-04-03 17:38:47 +0900

    FE-3018 feat: 모바일 QR 민팅 로직 작성

--- here-front/src/apis/register/useRegisterPaperNftQuery.ts
+++ here-front/src/apis/register/useRegisterPaperNftQuery.ts
@@ -2,15 +2,15 @@ import { NFT_SERVER_URL } from "@/utils/urls";
 import axios from "axios";
 import { useQuery } from "react-query";
 import * as queryKeys from "@/constants/queryKeys";
+import Swal from "sweetalert2";
+import "sweetalert2/dist/sweetalert2.min.css";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
 
 const fetcher = (memberId: string, serialNumber: string) =>
   axios
-    .get(NFT_SERVER_URL + `/nft/paper-bd-cert`, {
-      params: {
-        memberId: memberId,
-        serialNumber: serialNumber,
-      },
-    })
+    .get(NFT_SERVER_URL + `/nft/paper-bd-cert/${memberId}/${serialNumber}`)
     .then(({ data }) => {
       return data.data;
     });
@@ -26,7 +26,16 @@ const useRegisterPaperNftQuery = (memberId: string, serialNumber: string) => {
         console.log("Success");
       },
       onError: (err) => {
-        console.log("error!", err);
+        MySwal.fire({
+          icon: "error",
+          title: "등록된 헌혈 정보를 찾을 수 없습니다.",
+          showConfirmButton: false,
+          timer: 2000,
+          customClass: {
+            title: "text-20 font-medium",
+            popup: "w-440 h-260",
+          },
+        });
       },
     },
   );
