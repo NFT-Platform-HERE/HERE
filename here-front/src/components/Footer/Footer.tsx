e73cf13818bb09c11cb60c42f7531572cac78986
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-04-04 10:16:30 +0900

    FE-2019 feat: 모바일 footer 추가

--- here-front/src/components/Footer/Footer.tsx
+++ here-front/src/components/Footer/Footer.tsx
@@ -4,11 +4,14 @@ import { IoCallOutline } from "react-icons/io5";
 
 export default function Footer() {
   return (
-    <div className="flex h-300 w-full min-w-[1200px] justify-center bg-[#f7f7f7] text-14 font-light">
-      <div className="w-500">
-        <img src="images/logo.svg" className="mx-auto mt-100 w-150" />
+    <div className="flex h-300 w-full min-w-[1200px] flex-wrap justify-center bg-[#f7f7f7] text-14 font-light mobile:h-350 mobile:w-full mobile:min-w-300">
+      <div className="w-500 mobile:w-full">
+        <img
+          src="images/logo.svg"
+          className="mx-auto mt-100 w-150 mobile:mt-30 mobile:w-100"
+        />
       </div>
-      <div className="w-700 px-30 py-60">
+      <div className="w-700 px-30 py-60 mobile:w-full mobile:py-10 mobile:text-center">
         <p className="text-16">SSAFY 8기 특화 프로젝트 (디지털 화폐)</p>
         <div className="mt-20 leading-30">
           <RiVipCrownLine className="mr-5 inline-block text-25" />
