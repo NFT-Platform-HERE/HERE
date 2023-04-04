31c5b33c10d69e9877a74922106f86c6375561cb
ZoD <93974908+Yonghyunc@users.noreply.github.com>
2023-04-04 13:21:20 +0900

    FE-2019 feat: 헌혈의 집 선택 전 메시지 추가

--- here-front/src/features/Blood/BloodMap.tsx
+++ here-front/src/features/Blood/BloodMap.tsx
@@ -109,7 +109,7 @@ export default function BloodMap() {
   }, [geolocation]);
 
   return (
-    <div className="mx-auto flex h-500 w-350 gap-45 mobile:h-full mobile:flex-col">
+    <div className="mx-auto flex h-500 w-full gap-45 mobile:h-full mobile:w-350 mobile:flex-col">
       <div
         ref={mapElement}
         className="h-500 w-[calc(100%-425px)] mobile:h-[100vw] mobile:w-full"
@@ -119,7 +119,7 @@ export default function BloodMap() {
           헌혈의 집 정보
         </div>
         <div className="h-465 w-380 rounded-30 border-3 border-pen-5 bg-white mobile:h-325 mobile:w-full">
-          {bloodHouse && (
+          {bloodHouse ? (
             <div className="flex h-465 w-380 flex-col justify-center gap-20 pl-20 pr-20 mobile:mt-30 mobile:h-325 mobile:w-full">
               <div className="flex gap-13">
                 <img src="icons/house.png" className="h-25 w-25" />
@@ -163,6 +163,15 @@ export default function BloodMap() {
                   예약하러 가기
                 </CommonBtn>
               </div>
+            </div>
+          ) : (
+            <div className="pt-120 text-center mobile:pt-80">
+              <img
+                src="/icons/blood-house.png"
+                className="mx-auto mb-10 w-70"
+              />
+              <p>헌혈의 집 아이콘을 클릭하면</p>
+              <p>상세정보를 볼 수 있어요</p>
             </div>
           )}
         </div>
