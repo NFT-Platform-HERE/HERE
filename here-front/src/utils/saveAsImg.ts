f829fb244248020fa963308b8ce9373fc9c62dd7
skylove0911 <skylove0911@naver.com>
2023-04-05 21:38:24 +0900

    FE-1015 feat: 나의 NFT 이미지 저장하기

--- here-front/src/utils/saveAsImg.ts
+++ here-front/src/utils/saveAsImg.ts
@@ -1,27 +1,41 @@
 import { endSaveImg } from "@/stores/myNFT/saveImg";
 import * as htmlToImage from "html-to-image";
-import { useDispatch } from "react-redux";
 
 export const saveNFTImage = (id: string, dispatch: any) => {
   let width;
   let height;
-
-  const node = document.getElementsByClassName(id);
+  let node: any;
 
   if (node === null) {
     return;
   }
-  if (id === "relative overflow-hidden rounded-20 shadow-lg") {
+
+  if (id === "front-capture") {
+    node = document.querySelectorAll(".camera");
     width = 500;
     height = 809;
+    for (let i = 0; i < node.length; i++) {
+      htmlToImage
+        .toPng(node[i], {
+          canvasWidth: width,
+          canvasHeight: height,
+          cacheBust: true,
+        })
+        .then((dataUrl) => {
+          dispatch(endSaveImg());
+          const link = window.document.createElement("a");
+          link.download = "blood_donation.png";
+          link.href = dataUrl;
+          link.click();
+        })
+        .catch((err) => console.log(err));
+    }
   } else if (id === "back-capture") {
+    node = document.getElementById("back-capture");
     width = 566;
     height = 350;
-  }
-
-  for (let i = 0; i < node.length; i++) {
     htmlToImage
-      .toPng(node[i], {
+      .toPng(node, {
         canvasWidth: width,
         canvasHeight: height,
         cacheBust: true,
