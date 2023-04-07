883283fc03f30a08dc6c954c37281704e61bdb91
최정온 <chjo0330@gmail.com>
2023-04-07 09:19:45 +0900

    chore: SmartContract Address 초기화

--- here-front/src/utils/urls.ts
+++ here-front/src/utils/urls.ts
@@ -11,6 +11,6 @@ export const NOTIFICATION_SERVER_URL = "https://j8b209.p.ssafy.io:9013/api";
 
 export const IPFS_SERVER_URL = "https://j8b2091.p.ssafy.io/";
 
-export const HERE_CA = "0x77C082FA91eF2FA9c6D104a0Cc5Bf3b2006c8528";
+export const HERE_CA = "0x17E7F012F5AD87C423ca21654E36839ed0963941";
 
 export const IPFS_SERVER_METAINFO_URL = "https://ipfs.io/ipfs/";
